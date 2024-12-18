b0ac2ae684f76cc75006f366f79afdb9ca8d3e8e
Alekanedero <alexandrecompin@gmail.com>
2024-12-18 23:42:53 +0100

    # add list of courses

---
+++ app/admin/page.tsx
@@ -0,0 +1,22 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/layout";
+
+import Link from "next/link";
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent>
+        <Link href="/admin/courses">Courses</Link>
+      </LayoutContent>
+    </Layout>
+  );
+}

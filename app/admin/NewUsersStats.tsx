3f2a0ec1bbedb91d846cc595666a6162ba252ae8
Alekanedero <alexandrecompin@gmail.com>
2025-04-05 00:34:09 +0200

    #31 - fix to deploy 4

--- app/admin/NewUsersStats.tsx
+++ app/admin/NewUsersStats.tsx
@@ -1,3 +1,62 @@
+// import { getRequiredAuthSession } from "@/lib/auth";
+// import { prisma } from "@/lib/prisma";
+// import React from "react";
+// import { NewUserCharts } from "./NewUserCharts";
+
+// export const NewUsersStats = async () => {
+//   const session = await getRequiredAuthSession();
+
+//   const newUsers = await prisma.courseOnUser.findMany({
+//     where: {
+//       course: {
+//         creatorId: session.user.id,
+//       },
+//       createdAt: {
+//         // previous 30 days
+//         gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000),
+//       },
+//     },
+//     select: {
+//       createdAt: true,
+//       canceledAt: true,
+//       id: true,
+//     },
+//   });
+
+//   //formater les données pour rechart, il attend des tableau d'objet avec des clés et des valeurs
+//   const data = Array.from({ length: 30 }, (_, i) => {
+//     const date = new Date(new Date().setDate(new Date().getDate() - i));
+//     date.setHours(0, 0, 0, 0);
+
+//     const newUsersCount = newUsers.filter((user) => {
+//       const userDateWithoutTime = new Date(user.createdAt.setHours(0, 0, 0, 0));
+//       return userDateWithoutTime.getTime() === date.getTime();
+//     }).length;
+
+//     const canceledUsersCount = newUsers.filter((user) => {
+//       if (!user.canceledAt) {
+//         return false;
+//       }
+//       const userDate = new Date(user.canceledAt);
+//       const userDateWithoutTime = new Date(userDate.setHours(0, 0, 0, 0));
+
+//       return userDateWithoutTime.getTime() === date.getTime();
+//     }).length;
+
+//     return {
+//       date: date.toDateString(),
+//       newUsersCount,
+//       canceledUsersCount,
+//     };
+//   }).reverse();
+
+//   console.log({ data, newUsers });
+
+//   return <NewUserCharts data={data} />;
+// };
+
+// --------------- fix ti deploy ------------------
+
 import { getRequiredAuthSession } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import React from "react";
@@ -6,7 +65,7 @@ import { NewUserCharts } from "./NewUserCharts";
 export const NewUsersStats = async () => {
   const session = await getRequiredAuthSession();
 
-  const newUsers = await prisma.courseOnUser.findMany({
+  const newUsers = (await prisma.courseOnUser.findMany({
     where: {
       course: {
         creatorId: session.user.id,
@@ -21,17 +80,21 @@ export const NewUsersStats = async () => {
       canceledAt: true,
       id: true,
     },
-  });
+  })) as { createdAt: Date; canceledAt: Date | null; id: string }[];
 
   //formater les données pour rechart, il attend des tableau d'objet avec des clés et des valeurs
   const data = Array.from({ length: 30 }, (_, i) => {
     const date = new Date(new Date().setDate(new Date().getDate() - i));
     date.setHours(0, 0, 0, 0);
 
-    const newUsersCount = newUsers.filter((user) => {
-      const userDateWithoutTime = new Date(user.createdAt.setHours(0, 0, 0, 0));
-      return userDateWithoutTime.getTime() === date.getTime();
-    }).length;
+    const newUsersCount = newUsers.filter(
+      (user: { createdAt: Date; canceledAt: Date | null; id: string }) => {
+        const userDateWithoutTime = new Date(
+          user.createdAt.setHours(0, 0, 0, 0)
+        );
+        return userDateWithoutTime.getTime() === date.getTime();
+      }
+    ).length;
 
     const canceledUsersCount = newUsers.filter((user) => {
       if (!user.canceledAt) {
